f5b0aebb3990f3299d6499b33ff435c27bd380ba
rushikeshwayal <rushikeshwayal6@gmail.com>
2024-11-27 16:19:34 +0530

    updating tables investigator, sub investigator ,agency, sub agency , project status ,ProjectCoordinator

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, Admin
+from app.models import db, Admin , Investigator
 
 admin_bp = Blueprint('admin', __name__)
 @admin_bp.route('/test', methods=['GET'])
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import create_app
 from dotenv import load_dotenv
 import os
-
 load_dotenv()  # Ensure this is called early in the app
 
 app = create_app()
