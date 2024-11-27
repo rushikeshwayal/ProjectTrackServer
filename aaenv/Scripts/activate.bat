0420e130c4bb98fca1775699dd442519bd6f29d4
rohitdhadambe <rohitdhadambe@gmail.com>
2024-11-27 21:41:00 +0530

    Project_fund_table-(GET & POST API)

--- aaenv/Scripts/activate.bat
+++ aaenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python\ML\SQLalkemy\aaenv
+set VIRTUAL_ENV=D:\Project_track\ProjectTrackServer\aaenv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -14,18 +14,26 @@ def create_app():
     db.init_app(app)
 
     # Import models to register them with SQLAlchemy
-    from app.models import Admin , Investigator
+    from app.models import Admin, Investigator, Agency, ProjectFund
 
     # Create database tables if they don't exist
     with app.app_context():
         db.create_all()
 
+    # Register blueprints
+    register_blueprints(app)
+
+    return app
+
 def register_blueprints(app):
     from app.routes.admin import admin_bp
     from app.routes.investigator import investigator_bp
     from app.routes.agency import agency_bp
+    from app.routes.ProjectFund import project_fund_bp  # Import the new blueprint
+
     app.register_blueprint(admin_bp, url_prefix='/api')  # Example: prefix routes with /api
     app.register_blueprint(investigator_bp, url_prefix='/api')  # Example: prefix routes with /api
     app.register_blueprint(agency_bp, url_prefix='/api')  # Example: prefix routes with /api
+    app.register_blueprint(project_fund_bp, url_prefix='/api')  # Register Project Fund blueprint
 
     return app
