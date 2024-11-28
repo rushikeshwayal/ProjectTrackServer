f057e42b0ae941207c838bda850dc0361ebd821e
NirajBhirud <nirajbhirud4669@gmail.com>
2024-11-28 23:46:04 +0530

    get,post,put api for project table

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -33,6 +33,8 @@ def register_blueprints(app):
     from app.routes.SubAgency import sub_agency_bp
     from app.routes.ProjectFund import project_fund_bp  # Import the project_fund blueprint
     from app.routes.ProjectStatus import project_status_bp 
+    from app.routes.Project import project_bp
+
 
 
     # Register all blueprints with a common API prefix
@@ -43,6 +45,8 @@ def register_blueprints(app):
     app.register_blueprint(sub_agency_bp,url_prefix='/api')
     app.register_blueprint(project_fund_bp, url_prefix='/api')  
     app.register_blueprint(project_status_bp, url_prefix='/api')
+    app.register_blueprint(project_bp, url_prefix='/api')
+
     
 
 
