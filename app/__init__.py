f6caa870242ecc612da87118440c4b17ee3ee072
rushikeshwayal <rushikeshwayal6@gmail.com>
2024-11-29 12:59:03 +0530

    updating cores

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from app.extensions import db
 from app.config import Config
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
     app.config.from_object(Config)
 
     # Initialize the database
