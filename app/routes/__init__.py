8c69c2eee11d05b2bf2ff6ce490d009ef4eb7c07
rushikeshwayal <rushikeshwayal6@gmail.com>
2024-12-05 00:25:08 +0530

    Project Form Data

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -40,6 +40,9 @@ def register_blueprints(app):
     from app.routes.Project import project_bp
     from app.routes.ProjectCoordinator import projectcoordinator_bp
     from app.routes.message import message_bp
+    from app.routes.form import upload_bp
+
+
 
 
 
@@ -54,6 +57,8 @@ def register_blueprints(app):
     app.register_blueprint(project_bp, url_prefix='/api')
     app.register_blueprint(projectcoordinator_bp, url_prefix='/api')
     app.register_blueprint(message_bp, url_prefix='/api')
+    app.register_blueprint(upload_bp, url_prefix='/api')
+
 
 
     
